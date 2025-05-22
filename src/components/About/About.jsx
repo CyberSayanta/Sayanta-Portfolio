70413a77674d5b05d42a97b8dcdbed4a5f09d59f
CyberSayanta <sayantahaldar.cyberexpert@gmail.com>
2025-05-22 05:33:53 +0530

    1st Commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+import Tilt from "react-parallax-tilt";
+import profileImage from "../../assets/PNG.png";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            <pre>Sayanta Haldar</pre>
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I'm a </span>
+            <ReactTypingEffect
+              text={[
+                "Fullstack Developer",
+                "Cyber Security Analyst",
+                "Programmer",
+                "Software Engineer",
+              ]}
+              speed={80}
+              eraseSpeed={90}
+              typingDelay={400}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a motivated and versatile individual, always eager to take on
+            new challenges. With a passion for learning I am dedicated to
+            delivering high-quality results. With a positive attitude and a
+            growth mindset, I am ready to make a meaningful contribution and
+            achieve great things.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="src\assets\Sayanta Haldar_CV_2025.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Sayanta Haldar"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_50px_rgba(1300,69,236,1)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,325 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import AWS from "./assets/tech_logo/AWS.png";
+import KOTLIN from "./assets/tech_logo/KOTLIN.png";
+import JC from "./assets/tech_logo/JC.png";
+
+// Cyber Security Section Logo's
+import os from "./assets/tech_logo/os.png";
+import EH from "./assets/tech_logo/eh.png";
+import NS from "./assets/tech_logo/ns.png";
+import CG from "./assets/tech_logo/cg.png";
+import VAPT from "./assets/tech_logo/vapt.png";
+import CTI from "./assets/tech_logo/cti.png";
+import DF from "./assets/tech_logo/df.png";
+import SIR from "./assets/tech_logo/sir.png";
+
+// Tools Section Logo's
+import VC from "./assets/tech_logo/VC.png";
+import IT from "./assets/tech_logo/IT.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+import XML from "./assets/tech_logo/XML.png";
+import AS from "./assets/tech_logo/AS.png";
+import MO from "./assets/tech_logo/MO.png";
+
+// Experience Section Logo's
+import CCI from "./assets/company_logo/CCI.jpg";
+import IS from "./assets/company_logo/IS.jpg";
+import CSI from "./assets/company_logo/CSI.jpg";
+// Education Section Logo's
+import BCRAPC from "./assets/education_logo/BCRAPC.jpg";
+import YOUTH from "./assets/education_logo/YOUTH.jpg";
+import BKMHS from "./assets/education_logo/BKMHS.jpg";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Angular", logo: angularLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "GSAP", logo: gsapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Python", logo: pythonLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+      { name: "AWS", logo: AWS },
+    ],
+  },
+  {
+    title: "Cyber Security",
+    skills: [
+      { name: "Operating System", logo: os },
+      { name: "Ethical Hacking", logo: EH },
+      { name: "Network Security", logo: NS },
+      { name: "Crypto   graphy", logo: CG },
+      { name: "VAPT", logo: VAPT },
+      { name: "CTI", logo: CTI },
+      { name: "Digital Forensics", logo: DF },
+      { name: "Security Incident Response", logo: SIR },
+      { name: "Information Technology", logo: IT },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      { name: "JAVA", logo: javaLogo },
+      { name: "Kotlin", logo: KOTLIN },
+      { name: "Jetpack Compose", logo: JC },
+      { name: "XML", logo: XML },
+      { name: "Android Studio", logo: AS },
+      { name: "Microsoft Office", logo: MO },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Python", logo: pythonLogo },
+      { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+      { name: "Vibe Coding", logo: VC },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: IS,
+    role: "Security Admin Associate",
+    company: "Infotech Solutions",
+    date: "May 2025 - Jully 2025",
+    desc: "🔐 Collaborating with IT and development teams to secure systems and applications 🛡️ Performing security assessments using tools like OWASP ZAP and Burp Suite 📊 Participating in threat modeling, code reviews, and weekly security audits 📚 Continuously learning new cybersecurity methodologies, frameworks, and best practices",
+    skills: [
+      "Updated Soon",
+      // "CSS",
+      // "JavaScript",
+      // "React JS",
+      // "TypeScript",
+      // "Node JS",
+    ],
+  },
+  {
+    id: 1,
+    img: CCI,
+    role: "Cyber Security Intern",
+    company: "CODECRAFT INFOTECH",
+    date: "May 2025 - June 2025",
+    desc: "🛡️ This internship opportunity serves as an educational engagement, aimed at providing Sayanta with practical, real-world exposure to cybersecurity practices, tools, and protocols under the guidance of experienced professionals.",
+    skills: [
+      "Updated Soon",
+    ],
+  },
+  {
+    id: 2,
+    img: CSI,
+    role: "CSI Accredited Student",
+    company: "Computer Society of India",
+    date: "Feb 2025 - Feb 2026",
+    desc: "💻 This demonstrates their commitment to staying updated with industry trends and their proactive engagement with a community of IT experts, ensuring they are well-prepared for the challenges and opportunities of the global digital economy. We celebrate their initiative in building strong professional foundations.",
+    skills: [
+      "Updated Soon",
+    ],
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: BCRAPC,
+    school: "Dr. B.C. Roy Academy of Professional Courses, Durgapur",
+    date: "Aug 2023 - Jully 2027",
+    grade: "Pursuing",
+    desc: "I have completed my Bachelor's degree (BCA) in Computer Applications from Dr. B.C. Roy Academy of Professional Courses, Durgapur. During my time at Dr. B.C. Roy Academy of Professional Courses, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Dr. B.C. Roy Academy of Professional Courses has been instrumental in shaping my technical abilities and professional growth.",
+    degree: "Bachelor of Computer Applications - BCA",
+  },
+  {
+    id: 1,
+    img: YOUTH,
+    school: "Youth Computer Training Centre, Bishnupur",
+    date: "Apr 2022 - Mar 2023",
+    grade: "96% (A+)",
+    desc: "I completed my Diploma degree in Information Technology Application (DITA) from Youth Computer Training Centre, Bishnupur. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Computer Basics and Operating Systems to diving into MS Office Suite, Visual Basic (VB), Software Engineering Concepts and Database Management Systems, I gained practical insights into the world of software development. My time at Youth Computer Training Centre, allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Diploma in Information Technology Application - DITA",
+  },
+  {
+    id: 2,
+    img: BKMHS,
+    school: "Krittibas Mukherjee High School, Bishnupur",
+    date: "Feb 2021 - May 2023",
+    grade: "79.4% (A)",
+    desc: "I completed my class 12 education from Krittibas Mukherjee High School, Bishnupur, under the WBCHSE board, where I studied Biology, Nutration, and Geography with Computer Application.",
+    degree: "WBCHSE(XII) - Bio Science with Computer Application",
+  },
+  {
+    id: 3,
+    img: BKMHS,
+    school: "Krittibas Mukherjee High School, Bishnupur",
+    date: "Jan 2015 - July 2021",
+    grade: "72.42% (A)",
+    desc: "I completed my class 10 education from Krittibas Mukherjee High School, Bishnupur, under the WBBSE board, where I studied Science with Computer.",
+    degree: "WBBSE(X), Science with Computer Application",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "GitHub Profile Detective",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "CS Prep",
+    description:
+      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+    image: csprepLogo,
+    tags: [
+      "React JS",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    github: "https://github.com/codingmastr/CSPrep",
+    webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Movie Recommendation App",
+    description:
+      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/codingmastr/Movie-Recommendation-App",
+    webapp: "https://movie-recommendation-app-jet.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Email Validator NPM Package",
+    description:
+      "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+    image: npmLogo,
+    tags: ["React JS", "Node.js", "NPM", "Validation"],
+    github: "https://github.com/codingmastr/cmtk-email-validator",
+    webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  },
+  {
+    id: 4,
+    title: "Task Reminder Chrome Extension Tool",
+    description:
+      "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    image: taskremLogo,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/codingmastr/Task-Reminder-Tool",
+    webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  },
+  {
+    id: 5,
+    title: "Webverse Digital",
+    description:
+      "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+    image: webverLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    github: "https://github.com/codingmastr/Webverse-Digital",
+    webapp: "https://webversedigital.com/",
+  },
+  {
+    id: 6,
+    title: "Coding Master",
+    description:
+      "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    image: cmLogo,
+    tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    github: "https://codingmasterweb.in/",
+    webapp: "https://codingmasterweb.in/",
+  },
+  {
+    id: 7,
+    title: "Image Search App",
+    description:
+      "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    image: imagesearchLogo,
+    tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    github: "https://github.com/codingmastr/Image-Search-App",
+    webapp: "https://imagsearch.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Image Background Remover",
+    description:
+      "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    image: removebgLogo,
+    tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    github: "https://github.com/codingmastr/Image-Background-Remover",
+    webapp: "https://removeyourbg.netlify.app/",
+  },
+];
