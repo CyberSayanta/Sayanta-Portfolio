4af65905484c2272cf5ef4f6f9d292ee314d8f86
CyberSayanta <sayantahaldar.cyberexpert@gmail.com>
2025-06-03 06:12:50 +0530

    CV Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="src\assets\Sayanta Haldar_CV_2025.pdf"
+            href="https://drive.google.com/file/d/1U3bn25FZFFD1E2Urygxoh7hkyk5ndP35/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
